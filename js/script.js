8103404acf1b2b90cb5ff11f96b2f75e9259e4e9
manikandan <manimani19508@gmail.com>
2022-12-09 14:54:32 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
 // filter method
 
-const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-function isPrime(num) {
-  for (let i = 2; num > i; i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return num > 1;
-}
-console.log(array.filter(isPrime)); 
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// function isPrime(num) {
+//   for (let i = 2; num > i; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return num > 1;
+// }
+// console.log(array.filter(isPrime)); 
 
 
 
 // for each methods
 
-const words = ["one", "two",'jighhgr', "three", "four"];
-words.forEach((word) => {
-  console.log(word);
-  if (word === "two") {
-    words.shift(); 
-  }
-}); 
+// const words = ["one", "two",'jighhgr', "three", "four"];
+// words.forEach((word) => {
+//   console.log(word);
+//   if (word === "two") {
+//     words.shift(); 
+//   }
+// }); 
 
-console.log(words);
+// console.log(words);
 
 
 
@@ -32,67 +31,90 @@ console.log(words);
 // reduce mwthods
 
 
-let numbers = [1, 2, 3, 5];
-let sum = numbers.reduce(function (value1,value2) {
-    return  value1+value2
-});        
-console.log(sum);
+// let numbers = [1, 2, 3, 5];
+// let sum = numbers.reduce(function (value1,value2) {
+//     return  value1+value2
+// });        
+// console.log(sum);
 
 
 // index of method
-  var str = 'Departed Train';
-  var index = str.indexOf('train');
-  console.log(index);
+  // var str = 'Departed Train';
+  // var index = str.indexOf('train');
+  // console.log(index);
 
 
   // push method
-  let number = [10, 20, 30];
-  console.log(number);
-  let length = number.push(40);
-  console.log(number);
+  // let number = [10, 20, 30];
+  // console.log(number);
+  // let length = number.push(40);
+  // console.log(number);
 
 
   // pop method
-  let num = [10, 20, 30,100];
-  console.log(num);
-  let len = num.pop();
-  console.log(num);
+  // let num = [10, 20, 30,100];
+  // console.log(num);
+  // let len = num.pop();
+  // console.log(num);
 
 
 
   // shift method
-  var arr = [ "happy","GFG", "Geeks", "for", "Geeks"];
-  var value = arr.shift();
-  console.log(value);
-  console.log(arr);
-  console.log(arr.length)
+  // var arr = [ "happy","GFG", "Geeks", "for", "Geeks"];
+  // var value = arr.shift();
+  // console.log(value);
+  // console.log(arr);
+  // console.log(arr.length)
 
   // unshift method
-  const array1 = [1, 2, 3];
-console.log(array1.unshift(2, 1));
-console.log(array1);
+//   const array1 = [1, 2, 3];
+// console.log(array1.unshift(2, 1));
+// console.log(array1);
 
 
 // concat methods
 
-const str1 = 'black';
-const str2 = 'red';
-
-console.log(str1.concat(',', str2));
-console.log(str2.concat(', ', str1));
+// const str1 = 'black';
+// const str2 = 'red';
+// console.log(str1.concat(',', str2));
+// console.log(str2.concat(', ', str1));
 
 
 // every methods
 
 
-const ages = [32, 33, 19, 40];
+// const ages = [32, 33, 19, 40];
+// ages.every(checkAge)
+// function checkAge(age) {
+//   return age > 18;
+// }
+// console.log(ages.every(checkAge))
 
-ages.every(checkAge)
 
-function checkAge(age) {
-  return age > 18;
+
+// var num3=13;
+// var num4=0;
+// var temp=num3
+// while(num3<0){
+//   num3=++num3
+// }
+// console.log(temp)
+
+
+function myfun(){
+let num =[1,2,3,4,5,6]
+console.log(num[0])
+let num1=num[0]
+console.log(num1)
+let num2= parseInt(prompt("enter the value"))
+if(num1==num2){
+  console.log("same values")
 }
-console.log(ages.every(checkAge))
+else{
+  console.log("not a same number")
+}
+}
+document.getElementById("hi").innerHTML=""
 
 
 
@@ -101,6 +123,5 @@ console.log(ages.every(checkAge))
 
 
 
-let mani =[90,99,100,89,345]
-console.log(mani)
-mani[i]
+
+
